a6979d44ffc637ab66af36a2685b7b359d2974a5
dhalsdyd <minddong4992@gmail.com>
2025-01-06 23:02:39 +0900

    change index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import {ThemeProvider} from 'styled-components';
+import GlobalStyles from './component/style/GlobalStyle';
+import theme from './component/style/theme';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
