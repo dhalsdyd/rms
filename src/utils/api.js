153578ec18a4adc937f0bd034f3037d4f5347415
dhalsdyd <minddong4992@gmail.com>
2025-01-06 22:50:03 +0900

    Add Api code

---
+++ src/utils/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.example.com';
+
+const API = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+API.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error),
+);
+
+export default API;
